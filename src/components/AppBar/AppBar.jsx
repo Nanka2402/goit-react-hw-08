656773fa93a8ef8ce893019e2d21a9815982ae90
Nanka2402 <nastia.secr@gmail.com>
2024-04-19 22:58:01 +0300

    +

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,7 +3,7 @@ import css from "./AppBar.module.css";
 import { useAuth } from "../../hooks";
 import { Navigation } from "../Navigation/Navigation";
 import { UserMenu } from "../UserMenu/UserMenu";
-import { AuthNav } from "../../AuthNav/AuthNav";
+import { AuthNav } from "../AuthNav/AuthNav";
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
